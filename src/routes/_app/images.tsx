a3bfa0b306ab632a33befadccc61c9558b8c8ca9
elliot <elliothu.my@gmail.com>
2025-07-01 02:06:05 +0800

    feat: add image import functionality with UI enhancements for better user experience

--- src/routes/_app/images.tsx
+++ src/routes/_app/images.tsx
@@ -2,13 +2,15 @@ import { ImageDetail } from '@/components/image-detail';
 import { ImageItem } from '@/components/image-item';
 import { MetadataPreview } from '@/components/metadata-preview';
 import { Portal } from '@/components/portal';
-import { listImages } from '@/lib/bridge/images';
+import { listImages, loadImage } from '@/lib/bridge/images';
 import { Button } from '@heroui/button';
 import { useDisclosure } from '@heroui/modal';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { Info } from 'lucide-react';
+import { open } from '@tauri-apps/plugin-dialog';
+import { HardDriveDownload, Info } from 'lucide-react';
 import { useState } from 'react';
+import { toast } from 'sonner';
 
 export const Route = createFileRoute('/_app/images')({
   component: ImageList,
@@ -23,6 +25,31 @@ function ImageList() {
 
   const [current, setCurrent] = useState<string | null>(null);
 
+  const queryClient = useQueryClient();
+  const { mutate: importImage, isPending: isImporting } = useMutation({
+    mutationFn: async () => {
+      const path = await open({
+        title: 'Import an OCI archive',
+        filters: [
+          {
+            name: 'Image',
+            extensions: ['tar'],
+          },
+        ],
+      });
+      if (!path) {
+        return;
+      }
+      return await loadImage(path);
+    },
+    onSuccess: (image) => {
+      if (image) {
+        toast.success('Image imported');
+        void queryClient.refetchQueries({ queryKey: ['images'] });
+      }
+    },
+  });
+
   const disclosure = useDisclosure();
 
   const image = images?.find((i) => i.digest === current) ?? null;
@@ -30,8 +57,15 @@ function ImageList() {
   return (
     <>
       <Portal name="title">
-        <p>Images</p>
-        <p className="text-xs font-normal text-gray-400">{images?.length} images</p>
+        <div className="flex w-full items-center justify-between">
+          <div>
+            <p>Images</p>
+            <p className="text-xs font-normal text-gray-400">{images?.length} images</p>
+          </div>
+          <Button size="sm" variant="flat" onPress={() => importImage()} isIconOnly>
+            <HardDriveDownload size={18} />
+          </Button>
+        </div>
       </Portal>
 
       {images?.map((item) => (
