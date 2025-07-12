77a2fba603ce68e97356e250fdba313965fa7955
elliot <elliothu.my@gmail.com>
2025-07-12 13:00:55 +0800

    chore: publish 0.1.1

--- apps/trunktail/vite.config.ts
+++ apps/trunktail/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
@@ -41,7 +40,7 @@ export default defineConfig(() => ({
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ['**/src-tauri/**'],
+      ignored: ['**/src-tauri/**', '**/swift-lib/**'],
     },
   },
 }));
